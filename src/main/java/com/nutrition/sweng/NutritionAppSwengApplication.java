3d6399092aa04a7e7032da7011ff928d3af328d4
Lukas Behnke <lb440481@fh-muenster.de>
2022-03-30 16:59:56 +0200

    Spring initialized

---
+++ src/main/java/com/nutrition/sweng/NutritionAppSwengApplication.java
@@ -0,0 +1,13 @@
+package com.nutrition.sweng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionAppSwengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionAppSwengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutrition/sweng/NutritionAppSwengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrition.sweng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionAppSwengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
