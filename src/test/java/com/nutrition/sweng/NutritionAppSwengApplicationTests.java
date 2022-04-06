bf62dca6152375cc62e8502394b4a452d0ef0c32
Lukas Behnke <lb440481@fh-muenster.de>
2022-04-06 18:29:40 +0200

    added h2 database

--- src/test/java/com/nutrition/sweng/NutritionAppSwengApplicationTests.java
+++ src/test/java/com/nutrition/sweng/NutritionAppSwengApplicationTests.java
@@ -3,11 +3,11 @@ package com.nutrition.sweng;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class NutritionAppSwengApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
