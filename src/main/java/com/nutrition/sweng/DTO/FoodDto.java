5f9d7ff34560090c96f86d7ba8c12c604506b0c3
Lukas Behnke <lb440481@fh-muenster.de>
2022-05-16 14:13:57 +0200

    deleted getter and setter comments

--- src/main/java/com/nutrition/sweng/DTO/FoodDto.java
+++ src/main/java/com/nutrition/sweng/DTO/FoodDto.java
@@ -20,65 +20,32 @@ public class FoodDto {
         this.name = name;
         this.unitSize = unitSize.getUnitSize();
     }
-    /**
-     * Returns an Id of the FoodDto instance as a long datatype
-     *
-     * @return    Id of the FoodDto
-     *
-     * */
+
     public long getId() {
         return id;
     }
-    /**
-     * Sets the Id for the FoodDto instance as a long datatype
-     *
-     * @param   id   a long number
-     * @return none
-     *
-     * */
+
     public void setId(long id) {
         this.id = id;
     }
-    /**
-     * Returns the name of the FoodDto instance as a String datatype
-     *
-     * @return      name of the FoodDto
-     *
-     * */
+
 
     public String getName() {
         return name;
     }
 
-    /**
-     * Sets the name for the FoodDto instance as a String datatype
-     *
-     * @param   name   a String that names the FoodDto
-     * @return none
-     *
-     * */
+
 
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * Returns the unitSize of the FoodDto instance as a String datatype
-     *
-     * @return      unitSize of the FoodDto
-     *
-     * */
+
     public String getUnitSize() {
         return unitSize;
     }
 
-    /**
-     * Sets the name for the FoodDto instance as a String datatype
-     *
-     * @param   unitSize a String that defines the UnitSize of the FoodDto
-     * @return none
-     *
-     * */
+
     public void setUnitSize(String unitSize) {
         this.unitSize = unitSize;
     }
